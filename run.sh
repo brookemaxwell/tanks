c671c5822fa2b2537c0e3a6f2afb6b25d92aef68
Brooke Maxwell <brookeem4@gmail.com>
2014-05-03 20:29:40 -0600

    Ian's dumb_agent and run code

---
+++ run.sh
@@ -0,0 +1,10 @@
+./bin/bzrflag --world=maps/twoteams.bzw --friendly-fire --red-port=50100 --green-port=50101 --purple-port=50102 --blue-port=50103 $@ &
+sleep 2 
+#python bzagents/dumb_agent.py localhost 50100 &
+python bzagents/ian_dumb_agent.py localhost 50100
+#python ~cs470s/bzrflag/bots/compiled/blind.py agent0.pyc localhost 50101 #&
+#python bots/compiled/blind.py agent1 localhost 50101 &
+#python bots/compiled/blind.py agent1 localhost 50102 &
+#python bots/compiled/blind.py agent1 localhost 50103 &
+
+
