adc7226cd055d7c89b053d59b6081d0502d1996e
Ian Brendan Phillip Baenziger <ianbaen@fishing.cs.byu.edu>
2014-06-14 17:33:55 -0600

    updated run.sh and huntercontroller

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-./bin/bzrflag --world=maps/empty.bzw --friendly-fire --max-shots=3 --default-tanks=10 --default-posnoise=3 --red-port=50100 --green-port=50101  --time-limit=240 --respawn-time=240 --default-true-positive=.97 --default-true-negative=.9 --occgrid-width=100 --no-report-obstacles $@ &
+./bin/bzrflag --world=maps/box_two_teams.bzw --friendly-fire --max-shots=3 --default-tanks=10 --default-posnoise=3 --red-port=50100 --green-port=50101  --time-limit=240 --respawn-time=240 --default-true-positive=.97 --default-true-negative=.9 --occgrid-width=100 --no-report-obstacles $@ &
 sleep 2 
 python bzagents/combined_efforts/agent.py localhost 50100 &
 python bzagents/combined_efforts/agent.py localhost 50101
